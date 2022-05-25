1319606356edee71f9c88b8d99102fb2ae9ea937
Lazaro <lazaro@NarutoM11111111111>
2022-05-25 17:17:01 -0400

    more work on convert

--- convertiif.py
+++ convertiif.py
@@ -12,21 +12,18 @@ def error(trans):
 
 
 input_file_name = sys.argv[1]
-invnum = sys.argv[2]
-print(input_file_name, invnum)
+invnum = int(sys.argv[2])
 cust_file = os.path.join(PROJECT_ROOT, 'custs.csv')
-customers = {}
 product_file = os.path.join(PROJECT_ROOT, 'products.csv')
-products = {}
-invoice_dir = os.path.join(PROJECT_ROOT, '/invoices/')
+invoice_dir = os.path.join(PROJECT_ROOT, 'invoices/')
+customers = dict()
+products = dict()
+transactions = list()
+current_trans = list()
+customer = ""
+tmp_cust = ""
+total_amount = 0
 trans_date = ""
-check_num = 0
-curcust = ""
-tmpcust = ""
-prevcust = ""
-trans_type = ""
-product = ""
-trans_amount = 0
 
 head = "!TRNS	TRNSID	TRNSTYPE	DATE	ACCNT	NAME	CLASS	AMOUNT	DOCNUM	MEMO	CLEAR	TOPRINT	NAMEISTAXABLE	ADDR1	ADDR3	TERMS	SHIPVIA	SHIPDATE\r\n" \
 + "!SPL	SPLID	TRNSTYPE	DATE	ACCNT	NAME	CLASS	AMOUNT	DOCNUM	MEMO	CLEAR	QNTY	PRICE	INVITEM	TAXABLE	OTHER2	YEARTODATE	WAGEBASE\r\n" \
@@ -38,77 +35,92 @@ trans_end = "ENDTRNS\r\n"
 
 with open(cust_file, mode='r') as cst_file:
     reader = csv.reader(cst_file)
-    for rows in reader:
+    for row in reader:
         if reader.line_num == 1:
             continue
-        if rows[0] != "":
-            customers.update({rows[0]:rows[1]})
+        customers.update({row[0]:row[1]})
     cst_file.close()
 
 with open(product_file, mode='r') as prd_file:
     reader = csv.reader(prd_file)
-    for rows in reader:
+    for row in reader:
         if reader.line_num == 1:
             continue
-        if rows[0] != "":
-            products.update({rows[0]:rows[1]})
+        products.update({row[0]:row[1]})
     prd_file.close()
 
 with open(os.path.join(PROJECT_ROOT, input_file_name), mode='r') as our_file:
-    reader = csv.reader(our_file)
-    for rows in our_file:
-        print(rows.line_num)
-        if reader.line_num == 1:
-            try:
-                (check_num, trans_date) = rows
-                print(reader.line_num, rows.count)
-                assert(rows.count) == 2
-            except:
-                error(rows)
-                continue
+    transactions = list(csv.reader(our_file))
+    our_file.close()
 
-        if reader.line_num > 1:
-            try:
-                assert(rows.count == 3)
-            except:
-                error(rows)
-                continue
+for row in range(len(transactions)):
+    if row == 0:
+        continue
 
-        # try:
-        #     (curcust, trans_type, trans_amount) = list
+    current_trans = transactions[row]
+    tmp_cust = current_trans[0]
+    if tmp_cust != "":
+        customer = customers.get(tmp_cust, "")
+        if customer == "":
+            error(tmp_cust)
+            exit(1)
+        else:
+            total_amount += float(current_trans[2])
+            
 
-        # except:
-        #     error(trans)
-        #     continue
+    # reader = csv.reader(our_file)
+    # for rows in reader:
+    #     if reader.line_num == 1:
+    #         continue
 
-        # try:
-        #     tmpcust = customers.get(curcust)
-        #     assert(tmpcust != "")
-        # except:
-        #     error(trans)
-        #     continue
+    #     if reader.line_num == 2:
+    #         try:
+    #             check_num = int(rows[0])
+    #             trans_date = rows[1]
+    #             assert(rows[0] != 0)
+    #         except:
+    #             error(rows)
+    #             continue
 
-        # try:
-        #     product = products.get(trans_type)
-        #     assert(product != "")
-        # except:
-        #     error(trans)
-        #     continue
+    #     if reader.line_num > 2:
+    #         try:
+    #             curcust = rows[0]
+    #             trans_type = rows[1]
+    #             trans_amount = float(rows[2])
+    #             assert(curcust != "" and trans_type != "" and trans_amount != 0)
+    #         except:
+    #             error(rows)
+    #             continue
 
-        # if curcust != "" and prevcust =="":
-        #     prevcust = curcust
-        #     inv_file = open(invoice_dir + 'inv' + invnum + '.iif', mode = 'w')
-        #     inv_file.write(head)
-        #     inv_file.write(template1 %(trans_date, tmpcust, trans_amount, invnum, trans_date))
-        #     continue
-        
-        # if curcust == prevcust:
-        #     inv_file.write(template2 %(trans_date, product, trans_amount, trans_amount))
-        #     continue
-        
-        # if curcust != prevcust and prevcust != "":
-        #     inv_file.write(trans_end)
-        #     inv_file.close()
-        #     invnum += 1
-        #     prevcust = ""
-        #     continue
+    #         try:
+    #             trans_cust = customers.get(curcust)
+    #             assert(trans_cust != "")
+    #         except:
+    #             error(rows)
+    #             continue
+
+    #         try:
+    #             product = products.get(trans_type)
+    #             assert(product != "")
+    #         except:
+    #             error(rows)
+    #             continue
+
+    #         if curcust != "" and prevcust =="":
+    #             prevcust = curcust
+    #             inv_file = open(invoice_dir + 'inv' + str(invnum) + '.iif', mode = 'w')
+    #             inv_file.write(head)
+    #             inv_file.write(template1 %(trans_date, trans_cust, trans_amount, invnum, trans_date))
+    #             inv_file.write(template2 %(trans_date, product, trans_amount, trans_amount))
+    #             continue
+            
+    #         if curcust == prevcust:
+    #             inv_file.write(template2 %(trans_date, product, trans_amount, trans_amount))
+    #             continue
+            
+    #         if curcust != prevcust and prevcust != "":
+    #             inv_file.write(trans_end)
+    #             inv_file.close()
+    #             invnum += 1
+    #             prevcust = ""
+    #             continue
